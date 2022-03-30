cc4edbb3d3df1a0b0209e9a7b57657e4e1831322
Fabiola Aguilera <fabiaguilera96@gmail.com>
2022-03-29 21:12:43 -0400

    added Job entity

---
+++ JobSeed.Data/Job.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobSeed.Data
+{
+    public class Job
+    {
+        [Key]
+        public int JobId { get; set; }
+        
+        [Required]
+        public string Position { get; set; }
+        
+        [Required]
+        public string Company { get; set; }
+
+        [Required]
+        public string URL { get; set; }
+
+        [Required]
+        public decimal Salary { get; set; }
+
+        [Required]
+        public string Location { get; set; }
+
+        [Required]
+        public DateTimeOffset CreatedUtc { get; set; }
+        public DateTimeOffset? ModifiedUtc { get; set; }
+    }
+}
